269ae45ef19abfb1e9ef15f6e40adbb25e4ca655
Raymond Loman <raymond@carpago.nl>
2018-11-14 09:37:47 +0100

    Initial load

---
+++ src/main/java/nl/laurens/hockey/HockeyApplication.java
@@ -0,0 +1,12 @@
+package nl.laurens.hockey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HockeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HockeyApplication.class, args);
+	}
+}
---
+++ src/test/java/nl/laurens/hockey/HockeyApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.laurens.hockey;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HockeyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
