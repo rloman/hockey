89f6b481aa1e945b36c4953eaa507cb59d93e4a6
Raymond Loman <raymond@carpago.nl>
2018-11-15 15:33:30 +0100

    Finish the rest service

--- src/main/java/nl/laurens/hockey/rest/PlayerController.java
+++ src/main/java/nl/laurens/hockey/rest/PlayerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/players")
@@ -14,6 +15,12 @@ public class PlayerController {
     @Autowired
     private PlayerRepository playerRepository;
 
+
+    @PostMapping
+    public Player create(@RequestBody Player player) {
+
+        return this.playerRepository.save(player);
+    }
 
     @GetMapping
     public Iterable<Player> list() {
@@ -24,10 +31,45 @@ public class PlayerController {
 
     }
 
-    @PostMapping
-    public Player create(@RequestBody Player player) {
+    @GetMapping("{id}")
+    public Player get(@PathVariable long id) {
+        Optional<Player> optionalPlayer = this.playerRepository.findById(id);
 
-        return this.playerRepository.save(player);
+        if (optionalPlayer.isPresent()) {
+            return optionalPlayer.get();
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
+
+    @PutMapping("{id}")
+    public Player update(@PathVariable long id, @RequestBody Player player) {
+        Optional<Player> optionalPlayer = this.playerRepository.findById(id);
+
+        if (optionalPlayer.isPresent()) {
+
+            Player target = optionalPlayer.get();
+            target.setName(player.getName());
+            target.setPosition(player.getPosition());
+            target.setRugnummer(player.getRugnummer());
+
+            this.playerRepository.save(target);
+
+            return target;
+
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable long id) {
+        Optional<Player> optionalPlayer = this.playerRepository.findById(id);
+
+        if (optionalPlayer.isPresent()) {
+            this.playerRepository.deleteById(id);
+        }
+    }
+
 
 }
